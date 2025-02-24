c3d7ce13e8ff1659b53fa7d23eb85d67fedd8ed7
Abdullrahman <abdullrahmanwasfi@gmail.com>
2025-02-24 10:51:10 +0300

    af

--- github/variables.tf
+++ github/variables.tf
@@ -12,7 +12,7 @@ variable "github_owner" {
 variable "repository_name" {
   description = "Name of the repository to create"
   type        = string
-  default     = "MyNewRepo"
+  default     = "MyNewRepo6"
 }
 
 variable "repository_members" {
