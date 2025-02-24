df8f3ac95363a6edc0800cec92644d8f0df1ac4f
Abdullrahman <abdullrahmanwasfi@gmail.com>
2025-02-24 13:15:07 +0300

    new repo

--- github/variables.tf
+++ github/variables.tf
@@ -13,7 +13,7 @@ variable "github_owner" {
 variable "repository_name" {
   description = "Name of the repository to create"
   type        = string
-  default     = "MyNewRepo14336"
+  default     = "HelloRepo"
 }
 
 variable "repository_members" {
