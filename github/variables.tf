71efc210f95048ae80af891e721829ac65113436
Abdullrahman <abdullrahmanwasfi@gmail.com>
2025-02-24 13:41:57 +0300

    xovf

--- github/variables.tf
+++ github/variables.tf
@@ -13,7 +13,7 @@ variable "github_owner" {
 variable "repository_name" {
   description = "Name of the repository to create"
   type        = string
-  default     = "HelloRepo125"
+  default     = "HelloRepo1253"
 }
 
 variable "repository_members" {
