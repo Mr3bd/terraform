84f9731a77a491ee2f0051eee0125c2bcf9a1775
Abdullrahman <abdullrahmanwasfi@gmail.com>
2025-02-24 10:48:32 +0300

    edit

--- github/main.tf
+++ github/main.tf
@@ -1,11 +1,11 @@
 provider "github" {
-  token = var.G_TOKEN
-  owner = var.github_owner
+  token = var.G_TOKEN        # This should be the GitHub personal access token
+  owner = var.github_owner   # GitHub organization or user
 }
 
 # Create a new repository
 resource "github_repository" "repo" {
-  name        = var.repository_name
+  name        = var.repository_name   # Name of the repository to create
   description = "A new repository created with Terraform"
   visibility  = "private"
   auto_init   = true
@@ -13,16 +13,16 @@ resource "github_repository" "repo" {
 
 # Add members to the repository
 resource "github_repository_collaborator" "collaborator" {
-  for_each   = toset(var.repository_members)
-  repository = github_repository.repo.name
+  for_each   = toset(var.repository_members)  # List of GitHub usernames to add
+  repository = github_repository.repo.name    # The repository created above
   username   = each.value
-  permission = "push"
+  permission = "push"                         # Permissions (e.g., "push")
 }
 
 # Create branches
 resource "github_branch" "branch" {
-  for_each      = toset(var.repository_branches)
-  repository    = github_repository.repo.name
+  for_each      = toset(var.repository_branches)  # List of branches to create
+  repository    = github_repository.repo.name    # The repository created above
   branch        = each.value
-  source_branch = "main" # Default branch to create new branches from
+  source_branch = "main"  # Default branch to create new branches from
 }
--- github/variables.tf
+++ github/variables.tf
@@ -12,7 +12,7 @@ variable "github_owner" {
 variable "repository_name" {
   description = "Name of the repository to create"
   type        = string
-  default = "MyNewRepo5"
+  default     = "MyNewRepo"
 }
 
 variable "repository_members" {
